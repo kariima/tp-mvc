3c11a9e69258cbdfa0d07ee3da60e8acc5733c1d
karima <karima.lafria@gmail.com>
2019-07-04 09:47:57 +0200

    mise à jour controller

--- src/controller/MembresController.php
+++ src/controller/MembresController.php
@@ -1,5 +1,25 @@
 <?php
 
+class UtilisateursController
+{
+
+    public function index()
+    {
+
+        $utilisateurs = Utilisateur::findAll();
+        view('utilisateurs.index', compact('utilisateurs'));
+    }
+
+    public function show($id)
+    {
+
+        $utilisateur = Utilisateur::findOne($id);
+        view('utilisateurs.show', compact('utilisateur'));
+    }
+}
+
+/* <?php
+
 
 class ExamplesController {
 
@@ -29,4 +49,4 @@ class ExamplesController {
 
     }
 
-}
+} */
