ef705b6e22a14a53af459080faefeffde43751ae
cellia <cellia.m@live.fr>
2019-07-04 10:19:23 +0200

    update add + show utilisateurs en français

--- public/views/utilisateurs/add.php
+++ public/views/utilisateurs/add.php
@@ -6,17 +6,17 @@
 
 <form class="form-signin" method="post">
     <img class="mb-4" src="img/kindlogo.png" alt="logo kindle" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-    <label for="inputEmail" class="sr-only">Email address</label>
+    <h1 class="h3 mb-3 font-weight-normal">Connectez-vous</h1>
+    <label for="inputEmail" class="sr-only">E-mail</label>
     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
+    <label for="inputPassword" class="sr-only">Mot de passe</label>
     <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
     <div class="checkbox mb-3">
         <label>
-            <input type="checkbox" value="remember-me"> Remember me
+            <input type="checkbox" value="remember-me"> Se souvenir de moi
         </label>
     </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion </button>
     <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
 </form>
 
