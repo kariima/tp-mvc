50eaed2eb7915876afaea51e77cfb6f3f503be8e
Laure-Marie <lauremarie.torre@gmail.com>
2019-07-04 12:43:16 +0200

    enprunt

---
+++ src/model/emprunt.php
@@ -0,0 +1,78 @@
+<?php
+
+class Example extends Db {
+
+    const TABLE_NAME = "KINDLE";
+
+    protected $id;
+    protected $ArticleId;
+    protected $LecteurId;
+    protected $DateEmprunt;
+    protected $setRenouvellement;
+
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setArticleId($ArticleId) {
+        $this->ArticleId = $ArticleId;
+        return $this;
+    }
+
+    public function setLecteurId($LecteurId) {
+        $this->LecteurId = $LecteurId;
+        return $this;
+    }
+
+    public function setDateEmprunt($DateEmprunt) {
+        $dateFormat = DateTime::createFromFormat('d-m-Y', $DateEmprunt);
+        if (!$dateFormat) {
+                        throw new Exception('La date a un format incorrect.');
+                  }
+        $this->DateEmprunt = $DateEmprunt;
+        return $this;
+    }
+
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getArticleId() {
+        return $this->ArticleId;
+    }
+
+    public function getLecteurId() {
+        return $this->LecteurId;
+    }
+
+    public function getDateEmprunt() {
+        return $this->DateEmprunt;
+    }
+
+    public function getField4() {
+        return $this->field4;
+    }
+
+
+    public function save()
+    {
+        $data = [
+            "Id"    => $this->getId(),
+            "ArticleId"    => $this->getArticleId(),
+            "LecteurId"    => $this->getLecteurId(),
+            "DateEmprunt"    => $this->getDateEmprunt(),
+        ];
+        //if ($this->id > 0) return $this->update();
+        $nouvelId = Db::dbCreate(self::KINDLE, $data);
+        $this->setId($nouvelId);
+        return $this;
+    }
+
+    public static function findAll() {
+        $data = Db::dbFind(self::KINDLE);
+        return $data;
+    }
+}
