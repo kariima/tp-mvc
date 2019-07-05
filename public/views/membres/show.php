b3b97cde8ed7e8ebd050575f2395f835242febbe
Laure-Marie <lauremarie.torre@gmail.com>
2019-07-05 14:49:20 +0200

    formulaire profil maj

--- public/views/membres/show.php
+++ public/views/membres/show.php
@@ -1,100 +1,130 @@
 <?php ob_start(); ?>
 
-
-
 <div class="container">
     <h1 class='mb-5'>Votre compte Kindle</h1>
     
     <div class="row">
         <div class="col-12">
+        
 
     <!-- NAVIGATION -->
 
-            <ul class="nav nav-tabs bg-warning" id="myTab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" id="ID_TAB" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Votre profil Kindle</a>
-                </li>
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Profil</a>
+                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Emprunt</a>
+                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Coordonnées bancaires-Mots de passe</a>
+                </div>
+            </nav>
 
-                <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-                </li>
-            </ul>
-
-    <!-- TABLEAU -->
-            <div class="tab-content" id="myTabContent">
-                
+            <div class="tab-content border" id="nav-tabContent">
 
                 <!-- PANNEAU CARTE IDENTITE -->
-                
-                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
 
-                    <!-- PHOTO -->
-                    <div class="col-3">
-                        <div class="card mb-3" style="max-width: 540px;">
-                            <div class="row no-gutters">
-                                <img src="VINDRA_MONOI.jpg" class="card-img" alt="...">
-                            </div>
+            <div class="tab-pane fade show active mt-3" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                 <!-- PHOTO -->
+                 <div class="col-3">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                            <img src="<?= $membre['photo'] ?>" class="card-img" alt="...">
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-6">
-                        <table class="table">
-                            <tbody>
-                                <tr>
-                                <th scope="row">Prénom</th>
-                                <td><?= $membre['prenom'] ?></td>
-                                </tr>
+                <div class="col-6">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                            <th scope="row">Prénom</th>
+                            <td><?= $membre['prenom'] ?></td>
+                            <td><button class="btn btn-danger">Modifer</button></td>
+                            </tr>
 
-                                <tr>
-                                <th scope="row">Nom</th>
-                                <td><?= $membre['nom'] ?></td>
-                                </tr>
+                            <tr>
+                            <th scope="row">Nom</th>
+                            <td><?= $membre['nom'] ?></td>
+                            <td><button class="btn btn-danger">Modifer</button></td>
+                            </tr>
 
-                                <tr>
-                                <th scope="row">Email</th>
-                                <td><?= $membre['mail'] ?></td>
-                                </tr>
+                            <tr>
+                            <th scope="row">Email</th>
+                            <td><?= $membre['mail'] ?></td>
+                            <td><button class="btn btn-danger">Modifer</button></td>
+                            </tr>
 
-                                <tr>
-                                <th scope="row">Adresse</th>
-                                <td><?= $membre['adresse']?></td>
-                                </tr>
+                            <tr>
+                            <th scope="row">Adresse</th>
+                            <td><?= $membre['adresse']?></td>
+                            <td><button class="btn btn-danger">Modifer</button></td>
+                            </tr>
 
-                                <tr>
-                                <th scope="row">Ville</th>
-                                <td><?= $membre['ville']?></td>
-                                </tr>
+                            <tr>
+                            <th scope="row">Ville</th>
+                            <td><?= $membre['ville']?></td>
+                            <td><button class="btn btn-danger">Modifer</button></td>
+                            </tr>
 
-                                <tr>
-                                <th scope="row">Code Postal</th>    
-                                <td><?= $membre['cp']?></td>
-                                </tr>
-                            <tbody>
-                        </table>
-                    </div>
+                            <tr>
+                            <th scope="row">Code Postal</th>    
+                            <td><?= $membre['cp']?></td>
+                            <td><button class="btn btn-danger">Modifer</button></td>
+                            </tr>
+                        <tbody>
+                    </table>
                 </div>
 
             </div>
 
-                <!-- TABLEAU EMPRUNT -->
+                <!-- PANNEAU EMPRUNTS -->
 
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                <p>lalala</p>
+            <div class="tab-pane fade border" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+            <div class="col-6">
+                    <table class="table">
+                        <thead>
+                        <th scope="row">Oeuvre</th>
+                        <th scope="row">Date Emprunt</th>
+                        <th scope="row">Nombres de Renouvellements</th>
+                        <th scope="row">Renouveller</th>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                            <td><?= $emprunt['Titre']?> de <?= $emprunt['Auteur']?> </td>
+                            <td><?= $emprunt['date_emprunt']?></td>
+                            <td><?= $emprunt['renouvellement_emprunt']?></td>
+                            <td><button class="btn btn-danger">Renouveller</button></td>
+                            </tr>
+                        <tbody>
+                    </table>
+                </div>
+
             </div>
 
-            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                <p>lalala</p>
+                <!-- CARTE BANCAIRE - MOTS DE PASSE -->
+            <div class="tab-pane fade mt-3" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                 <table class="table">
+                     <tbody>       
+                        <tr>
+                            <th scope="row">Carte Bancaire</th>
+                            <td><?= $membre['carte_bancaire']?></td>
+                            <td><button class="btn btn-primary">Montrer</button></td>
+                            <td><button class="btn btn-danger">Modifer</button></td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">Mot de passe</th>    
+                            <td><?= $membre['mot_de_Passe']?></td>
+                            <td><button class="btn btn-primary">Montrer</button></td>
+                            <td><button class="btn btn-danger">Modifier</button></td>
+                        </tr>
+                    <tbody>
+                </table>
             </div>
-
-
-        <!-- TABLEAU COORDONEES BANCAIRES -->
-
+        
         </div>
     </div>
 </div>
+
 
 
 
