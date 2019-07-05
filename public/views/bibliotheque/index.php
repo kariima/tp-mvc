2c40930dad6956310febcf78aca93dab348480db
karima <karima.lafria@gmail.com>
2019-07-05 12:08:13 +0200

    bibliotheque

--- public/views/bibliotheque/index.php
+++ public/views/bibliotheque/index.php
@@ -2,23 +2,71 @@
 
 
 <body>
-    <h1>Liste des livres</h1>
-    <table>
-        <tr>
-            <th>Titre :</th>
-            <th>Couverture :</th>
-            <th>Auteur :</th>
+    <h1>Liste bibliothèque :</h1>
+    <!-- <table>
+        <tr class="m-2">
+            <th class="card-header">Titre</th>
+            <th class="card-header">Auteur </th>
+            <th class="card-header">Résumé </th>
+            <th class="card-header">Catégorie </th>
+            <th class="card-header">Genre </th>
+            <th class="card-header">Délai </th>
+            <th class="card-header">Prix </th>
+            <th class="card-header">Date de parution </th>
+            <th class="card-header">Couverture </th>
         </tr>
 
-        <tr>
-            <td> <?= ($_GET['titre']) ?></td>
-            <td> <?= ($_GET['couverture']) ?>;</td>
-            <td> <?= ($_GET['auteur']) ?>;</td>
-        </tr>
+
+
+        <?php foreach ($bibliotheque as $b) : ?>
+                        <tr class="card-body mt-2">
+                            <td class="card-text m-2"><?= $b['titre'] ?></td>
+                            <td class="card-text m-2"><?= $b['auteur'] ?></td>
+                            <td class="card-text m-2"><?= $b['resume'] ?></td>
+                            <td class="card-text m-2"><?= $b['categorie'] ?></td>
+                            <td class="card-text m-2"><?= $b['genre'] ?></td>
+                            <td class="card-text m-2"><?= $b['delai'] ?></td>
+                            <td class="card-text m-2"><?= $b['prix'] ?></td>
+                            <td class="card-text m-2"><?= $b['date_de_parution'] ?></td>
+                            <td class="card-text m-2"><?= $b['couverture'] ?></td>
+                        </tr>
+
+        <?php endforeach; ?>
+    </table> -->
+
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th class="card-header">Titre</th>
+                <th class="card-header">Couverture </th>
+                <th class="card-header">Auteur </th>
+                <th class="card-header">Résumé </th>
+                <th class="card-header">Catégorie </th>
+                <th class="card-header">Genre </th>
+                <th class="card-header">Délai </th>
+                <th class="card-header">Prix </th>
+                <th class="card-header">Date de parution </th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($bibliotheque as $b) : ?>
+                <tr class="card-body mt-2">
+                    <td class="card-text m-2"><?= $b['titre'] ?></td>
+                    <td class="card-text m-2"><?= $b['couverture'] ?></td>
+                    <td class="card-text m-2"><?= $b['auteur'] ?></td>
+                    <td class="card-text m-2"><?= substr($b['resume'], 0, 30) . "..." ?></td>
+                    <td class="card-text m-2"><?= $b['categorie'] ?></td>
+                    <td class="card-text m-2"><?= $b['genre'] ?></td>
+                    <td class="card-text m-2"><?= $b['delai'] ?></td>
+                    <td class="card-text m-2"><?= $b['prix'] ?></td>
+                    <td class="card-text m-2"><?= $b['date_de_parution'] ?></td>
+                </tr>
+
+            <?php endforeach; ?>
+        </tbody>
     </table>
 
-
-
+    <a href="<?= url('ajout-bibliotheque') ?>" class="btn btn-danger btn-sm mb-2">Ajouter une oeuvre</a>
 
 
 
